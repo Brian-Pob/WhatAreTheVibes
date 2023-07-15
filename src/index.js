fbee895336208b49786f88b7feb4e8cbb45e8388
Brian Michael Poblete <brianmp.dev@gmail.com>
2023-07-14 22:40:28 -0400

    more stuff

--- src/index.js
+++ src/index.js
@@ -1,58 +1,75 @@
-import "./styles.css";
+import './styles.css';
 
 function getData() {
   const vibes = [
-    "Blissful",
-    "Bold",
-    "Captivating",
-    "Carefree",
-    "Courageous",
-    "Cozy",
-    "Curious",
-    "Dreamy",
-    "Dynamic",
-    "Effervescent",
-    "Empathetic",
-    "Empowered",
-    "Enchanting",
-    "Enigmatic",
-    "Ethereal",
-    "Euphoric",
-    "Exciting",
-    "Graceful",
-    "Grounded",
-    "Harmonious",
-    "Inspiring",
-    "Intimate",
-    "Intriguing",
-    "Jubilant",
-    "Lively",
-    "Magical",
-    "Majestic",
-    "Melancholic",
-    "Mystical",
-    "Nostalgic",
-    "Nurturing",
-    "Optimistic",
-    "Peaceful",
-    "Playful",
-    "Quirky",
-    "Radiant",
-    "Reflective",
-    "Refreshing",
-    "Resilient",
-    "Rustic",
-    "Sentimental",
-    "Serendipitous",
-    "Sultry",
-    "Tranquil",
-    "Vibrant",
-    "Whimsical",
-    "Wistful"
+    'Blissful',
+    'Bold',
+    'Captivating',
+    'Carefree',
+    'Courageous',
+    'Cozy',
+    'Curious',
+    'Dreamy',
+    'Dynamic',
+    'Effervescent',
+    'Empathetic',
+    'Empowered',
+    'Enchanting',
+    'Enigmatic',
+    'Ethereal',
+    'Euphoric',
+    'Exciting',
+    'Graceful',
+    'Grounded',
+    'Harmonious',
+    'Inspiring',
+    'Intimate',
+    'Intriguing',
+    'Jubilant',
+    'Lively',
+    'Magical',
+    'Majestic',
+    'Melancholic',
+    'Mystical',
+    'Nostalgic',
+    'Nurturing',
+    'Optimistic',
+    'Peaceful',
+    'Playful',
+    'Quirky',
+    'Radiant',
+    'Reflective',
+    'Refreshing',
+    'Resilient',
+    'Rustic',
+    'Sentimental',
+    'Serendipitous',
+    'Sultry',
+    'Tranquil',
+    'Vibrant',
+    'Whimsical',
+    'Wistful',
   ];
   return vibes[Math.floor(Math.random() * vibes.length)];
 }
-document.getElementById("app").innerHTML = `<pre>
+document.getElementById('app').innerHTML = `<pre>
 ${getData()}
 </pre>
 `;
+
+// Get a reference to the <head> element
+function setTitle() {
+  // Get a reference to the <head> element
+  var head = document.getElementsByTagName('head')[0];
+
+  // Create a new <meta> element for OpenGraph title
+  var ogTitle = document.createElement('meta');
+
+  // Set the attributes for the OpenGraph title
+  ogTitle.setAttribute('property', 'og:title');
+  ogTitle.setAttribute('content', 'Your dynamically generated title goes here');
+
+  // Append the OpenGraph title to the <head> element
+  head.appendChild(ogTitle);
+}
+document.onload(setTitle());
