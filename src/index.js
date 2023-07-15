679649c6388b9eeb640db5bc187f382dcf1e464b
Brian Michael Poblete <brianmp.dev@gmail.com>
2023-07-14 22:31:48 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import "./styles.css";
+
+function getData() {
+  const vibes = [
+    "Blissful",
+    "Bold",
+    "Captivating",
+    "Carefree",
+    "Courageous",
+    "Cozy",
+    "Curious",
+    "Dreamy",
+    "Dynamic",
+    "Effervescent",
+    "Empathetic",
+    "Empowered",
+    "Enchanting",
+    "Enigmatic",
+    "Ethereal",
+    "Euphoric",
+    "Exciting",
+    "Graceful",
+    "Grounded",
+    "Harmonious",
+    "Inspiring",
+    "Intimate",
+    "Intriguing",
+    "Jubilant",
+    "Lively",
+    "Magical",
+    "Majestic",
+    "Melancholic",
+    "Mystical",
+    "Nostalgic",
+    "Nurturing",
+    "Optimistic",
+    "Peaceful",
+    "Playful",
+    "Quirky",
+    "Radiant",
+    "Reflective",
+    "Refreshing",
+    "Resilient",
+    "Rustic",
+    "Sentimental",
+    "Serendipitous",
+    "Sultry",
+    "Tranquil",
+    "Vibrant",
+    "Whimsical",
+    "Wistful"
+  ];
+  return vibes[Math.floor(Math.random() * vibes.length)];
+}
+document.getElementById("app").innerHTML = `<pre>
+${getData()}
+</pre>
+`;
