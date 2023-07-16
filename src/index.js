e0b67de66dac814609effad9f7b5203b7b8db585
Brian Michael Poblete <brianmp.dev@gmail.com>
2023-07-15 21:24:59 -0400

    fix title

--- src/index.js
+++ src/index.js
@@ -56,19 +56,3 @@ document.getElementById('app').innerHTML = `<pre>
 ${getData()}
 </pre>
 `;
-
-// Get a reference to the <head> element
-window.onload = () => {
-  // Get a reference to the <head> element
-  var head = document.getElementsByTagName('head')[0];
-
-  // Create a new <meta> element for OpenGraph title
-  var ogTitle = document.createElement('meta');
-
-  // Set the attributes for the OpenGraph title
-  ogTitle.setAttribute('property', 'og:title');
-  ogTitle.setAttribute('content', getData());
-
-  // Append the OpenGraph title to the <head> element
-  head.appendChild(ogTitle);
-};
